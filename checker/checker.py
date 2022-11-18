d3dd1a62bc784ce1d7d4c834862769c2f9d4dd71
Рогозов Михаил <mv.rogozov@yandex.ru>
2022-11-18 09:57:12 +0300

    refactored. threading add

--- checker/checker.py
+++ checker/checker.py
@@ -3,6 +3,7 @@ import argparse
 import re
 
 import requests
+import threading
 
 METHODS = {
     'get': requests.get,
@@ -14,42 +15,66 @@ METHODS = {
     'head': requests.head
 }
 
+result = {}
 
-def check_urls(filename=''):
 
+def func_threads(url):
+    checking_threads = []
+    for name, method in METHODS.items():
+        checking_thread = threading.Thread(
+            target=check_url, args=(url, name, method)
+        )
+        checking_threads.append(checking_thread)
+        checking_thread.start()
+    for thread in checking_threads:
+        thread.join()
+
+
+def check_url(url, name, method):
+    result[url] = {}
+    response = None
+    if url.startswith('http'):
+        try:
+            response = method(url)
+        except Exception:
+            pass
+    else:
+        try:
+            response = method(f'http://{url}')
+            result[url][name] = response.status_code
+        except Exception:
+            pass
+    if response:
+        if response.status_code != 405:
+            result[url][name] = response.status_code
+
+
+def check_in_thread(filename=''):
     url_regex = re.compile(
         r'((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){2,6}'
         r'([a-zA-Z0-9\.\&\/\?\:@\-_=#])*'
     )
-
-    result = {}
     try:
         with open(filename, 'r') as file:
             print('Please wait...')
             for line in file:
                 url = url_regex.search(line)
                 if url:
-                    result[url.group()] = {}
-                    for key, value in METHODS.items():
-                        response = None
-                        if line.startswith('http'):
-                            try:
-                                response = value(url.group())
-                            except Exception:
-                                pass
-                        else:
-                            try:
-                                response = value(f'http://{url.group()}')
-                            except Exception:
-                                pass
-                        if response:
-                            if response.status_code != 405:
-                                result[url.group()][key] = response.status_code
+                    func_threads(url.group())
                 else:
                     print(f'Строка "{line}" не является ссылкой')
     except FileNotFoundError:
         return('No such file or directory')
-    return result
+    sorted_result = sort_result(result)
+    return sorted_result
+
+
+def sort_result(result):
+    new_result = {}
+    for key, value in result.items():
+        value = dict(sorted(value.items()))
+        new_result[key] = value
+    return new_result
 
 
 def main():
@@ -57,7 +82,7 @@ def main():
     parser.add_argument('filename', type=str, help='file contains urls')
     args = parser.parse_args()
     filename = args.filename
-    print(check_urls(filename))
+    print(check_in_thread(filename))
 
 
 if __name__ == '__main__':
--- tests.py
+++ tests.py
@@ -1,9 +1,9 @@
-from checker.checker import check_urls
+from checker.checker import check_in_thread, sort_result
 
 
 def test_no_args():
     """Без передачи параметров."""
-    result = check_urls()
+    result = check_in_thread()
     right_result = 'No such file or directory'
     assert result == right_result, (
         'Ошибка запуска без параметров.'
@@ -13,29 +13,54 @@ def test_no_args():
 def test_file():
     """Тестовый файл"""
     filename = 'fixture_file.txt'
-    result = check_urls(filename)
+    result = check_in_thread(filename)
     right_result = (
         {'https://stackoverflow.com/':
-            {'get': 200,
+            {
+             'delete': 200,
+             'get': 200,
+             'head': 200,
+             'options': 200,
+             'patch': 200,
              'post': 200,
              'put': 200,
-             'patch': 200,
-             'options': 200,
-             'delete': 200,
-             'head': 200},
+             },
          'www.avito.ru':
-            {'head': 301},
+            {
+             'delete': 403,
+             'get': 403,
+             'head': 301,
+             'options': 403,
+             'patch': 403,
+             'post': 403,
+             'put': 403,
+             },
          'strange_string.sd': {},
          'gmail.com':
-            {'get': 200,
+            {
+             'delete': 200,
+             'get': 200,
+             'head': 301,
+             'options': 200,
+             'patch': 200,
              'post': 200,
              'put': 200,
-             'patch': 200,
-             'options': 200,
-             'delete': 200,
-             'head': 301}
+             }
          }
     )
     assert result == right_result, (
         'Неправильный результат работы.'
     )
+
+
+def test_sort_result():
+    test_dict = {
+        'aaa': {'v': 4, 'z': 1, 'a': 3},
+        'abb': {'d': 14, 'gz': 21, 'ga': 3}
+    }
+    right_result = {
+        'aaa': {'a': 3, 'v': 4, 'z': 1},
+        'abb': {'d': 14, 'ga': 3, 'gz': 21}
+    }
+    result = sort_result(test_dict)
+    assert result == right_result
